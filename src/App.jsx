7866eafeaa634f890f400be12363a71205894d5d
nethmalgeesaraux <nethmalpaypal@gmail.com>
2026-03-19 21:13:14 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-blue-500">
+      Tailwind Working 🚀
+    </h1>
+    </div>
+  )
+}
+
+export default App
